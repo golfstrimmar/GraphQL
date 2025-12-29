3d8703b8e27a208d9e4131370e47c867362c4506
golfstrimmar <yushinbox@gmail.com>
2025-12-29 17:26:09 +0100

    ++

---
+++ frontend/src/app/plaza/PlazaComponent/PageHeader.tsx
@@ -0,0 +1,26 @@
+import ProjectsIcon from "@/components/icons/ProjectsIcon";
+import WorkerIcon from "@/components/icons/WorkerIcon";
+import SundboxIcon from "@/components/icons/SundboxIcon";
+
+type PageHeaderProps = {
+  iconName: string;
+  title: string;
+};
+
+function PageHeader(iconName, title) {
+  return (
+    <div className="flex items-center gap-3 mb-6">
+      <div className="flex items-center gap-3">
+        <div className="w-6 h-6 bg-gradient-to-br from-blue-500 to-cyan-500 rounded-lg flex items-center justify-center">
+          <span className="text-white">
+            {iconName === "projectsIcon" ? <ProjectsIcon /> : null}
+            {iconName === "canvasIcon" ? <WorkerIcon /> : null}
+            {iconName === "sandboxIcon" ? <SundboxIcon /> : null}
+          </span>
+        </div>
+        <h6 className="font-bold text-slate-800">{title}</h6>
+      </div>
+    </div>
+  );
+}
+export default PageHeader;
