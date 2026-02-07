845caf09d5b454d8d015ded36246a4b70439306c
golfstrimmar <yushinbox@gmail.com>
2026-02-07 12:50:07 +0100

    +кнопки

--- frontend/src/app/design/ListDesignSystems.tsx
+++ frontend/src/app/design/ListDesignSystems.tsx
@@ -16,36 +16,13 @@ import type { HtmlNode } from "@/types/HtmlNode";
 import type { DesignSystem } from "@/types/DesignSystem";
 import { useRouter } from "next/navigation";
 import ClearIcon from "@/components/icons/ClearIcon";
+import { CONTENT, dTN, dBT } from "@/app/design/defaults";
 // -----------
 const ModalCreateDesignSystem = dynamic(
   () => import("./ModalCreateDesignSystem"),
   { ssr: false, loading: () => <Loading /> },
 );
-const CONTENT = "The quick brown fox jumps over the lazy dog.";
-const dBT = [
-  "font-size: 14px;color: #64ffda;font-weight: 400;line-height: 1;font-family: 'Montserrat', sans-serif;display: inline-flex;align-items: center;justify-content: center;cursor: pointer;gap: 0.5rem;padding: 0.25rem 0.5rem;border-radius: 0.25rem;transition: all 200ms ease-in-out;box-shadow: 0 1px 2px rgba(15, 23, 42, 0.08);border: 1px solid transparent;border-color: #64ffda; &:hover {color: #000000;border-color: #64ffda;background-color: #64ffdb86;}; &:active {transform: translateY(1px);background-color: #64ffda;}",
-  "font-size: 16px; color: #e6f1ff; font-weight: 500; line-height: 1; font-family:  'Montserrat', sans-serif; display: inline-flex; align-items: center; justify-content: center; cursor: pointer; gap: 0.5rem; padding: 0.25rem 0.5rem; border-radius: 0.25rem; transition: all 200ms ease-in-out; box-shadow: 0 1px 2px rgba(15, 23, 42, 0.08); border-color: #4d6a92; background-color: #4d6a92; border: 1px solid #4d6a92; &:hover { border-color: #112240; background-color: #233554; } &:active { transform: translateY(1px); background-color: #64ffda; }",
-  "font-size:16px; color:#000000; font-weight:500; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:18px; color:#000000; font-weight:400; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:20px; color:#000000; font-weight:500; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:22px; color:#000000; font-weight:600; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:24px; color:#000000; font-weight:700; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:26px; color:#000000; font-weight:800; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:28px; color:#000000; font-weight:800; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:30px; color:#000000; font-weight:900; line-height:1; font-family:'Montserrat', sans-serif;",
-];
-const dTN = [
-  "font-size:12px;  color: #000000; font-weight:300; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:14px; color:#000000; font-weight:400; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:16px; color:#000000; font-weight:400; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:18px; color:#000000; font-weight:400; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:20px; color:#000000; font-weight:500; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:22px; color:#000000; font-weight:600; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:24px; color:#000000; font-weight:700; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:26px; color:#000000; font-weight:800; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:28px; color:#000000; font-weight:800; line-height:1; font-family:'Montserrat', sans-serif;",
-  "font-size:30px; color:#000000; font-weight:900; line-height:1; font-family:'Montserrat', sans-serif;",
-];
+
 type Text = {
   tagName: string;
   className: string;
